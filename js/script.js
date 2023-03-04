092757b7c091828ac30b650ce3efa7d81a48f680
Samuel Tobias Roland Uyet <samu1493@stud.kea.dk>
2023-03-04 11:50:27 +0100

    socials on side added and hover on nac links added

--- js/script.js
+++ js/script.js
@@ -2,16 +2,22 @@
 
 // variabler til burger menu
 const burger = document.querySelector("nav button");
+const toTop = document.querySelector(".to_top");
 const menu = document.querySelector(".nav-menu");
 
 window.addEventListener("load", start);
 
 function start() {
   burger.addEventListener("click", activeteBurgerMenu);
+  toTop.addEventListener("click", scrollToTop);
 }
 
 function activeteBurgerMenu() {
   console.log("activeteBurgerMenu");
   burger.classList.toggle("active");
   menu.classList.toggle("active");
+}
+
+function scrollToTop() {
+  window.scrollTo(0, 0);
 }
